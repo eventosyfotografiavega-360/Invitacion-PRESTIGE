f9f5e07f162b836450a7fa3dbec020a2a57e5c88
eventosyfotografiavega-360 <eventosyfotografia.vega@gmail.com>
2026-04-22 17:12:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 function openEnvelope() {
     const wrapper = document.getElementById('envelope-wrapper');
     const content = document.getElementById('main-content');
-    
+    const musica = document.getElementById('miMusica');
+
+    wrapper.style.transform = 'translateY(-100%)';
     wrapper.style.opacity = '0';
+    
     setTimeout(() => {
-        wrapper.style.visibility = 'hidden';
-        content.style.opacity = '1'; // Esto hace que el contenido aparezca suavemente
-        // Si tienes música, aquí es donde debe empezar
-        if (typeof musica !== 'undefined') musica.play();
+        wrapper.style.display = 'none';
+        content.style.opacity = '1';
+        if (musica) musica.play();
     }, 1000);
 }
 
 function toggleMenu() {
     document.getElementById('side-menu').classList.toggle('menu-visible');
 }
-
-// Inicializar el slider de Padrinos
-const swiper = new Swiper('.swiper-padrinos', {
-    pagination: { el: '.swiper-pagination' },
-    loop: true,
-});
